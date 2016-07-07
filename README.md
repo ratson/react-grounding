73a97fe0f52369224da61f6c4265612982eff604
Ratson <github@ratson.name>
2016-07-07 23:45:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# react-grounding
+
+Like react-headroom, but at the bottom.
+
+## Installation
+
+```
+npm install react-grounding --save
+```
+
+## Usage
+
+```js
+import Grounding from 'react-grounding'
+
+<Grounding>
+  <footer>I am on the ground.</footer>
+</Grounding>
+```
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Headroom from 'react-headroom'
+
+class Grounding extends Headroom {
+  constructor(...args) {
+    super(...args)
+
+    const superUnpin = this.unpin.bind(this)
+    this.unpin = () => {
+      superUnpin()
+      this.setState({
+        translateY: 0,
+      })
+    }
+  }
+
+  componentDidMount() {
+    super.componentDidMount()
+
+    this.pin()
+  }
+}
